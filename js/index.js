dbb3f14c3bcf41e1a1f1fa96576a3364a2c60863
Furkan YILDIRIM <141413430+furkiyildirim@users.noreply.github.com>
2023-08-24 16:56:37 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const words =
+`||COMPBIO (C) 1991 Motherboard, Inc.||
+BIOS Date 01/24/92 15:43:029 Ver:  00.00.15||
+CPU: Intel (R) CPU 330 @ 40 MHz|
+Speed:  40 MHz||
+This VGA/PCI Bios is released under the GNU LGPL||
+|
+Memory Clock:  64MHz, Tcl:7 Trcd:4 Trp:8 Tras:20 (2T Timing) 8 bit||
+Memory Test:  128420K || OK|
+
+||PMU ROM Version:  9303|
+|NVMM ROM Version:  4.092.88|
+Initalizing USB Controllers|.|. ||Done.
+||128MB |OK||
+USB Device(s):  1 Keyboard|||, 1 Mouse||, 1 Hub|, 1 Storge Device|
+Auto-detecting USB Mass Storge Devices|.|.||
+Device #01:  USB 2.0 FlashDisk *Speed*||
+01 USB mass storge devices found and configured||
+(C) Motherboard, Inc.|
+64-0100-00001-001011111-092909-79297-1AE0V003-Y2UC|
+Booting from Hard Disk|.|||.||.||`;
+
+    const outputElement = document.getElementById('output');
+    
+    let i = 0;
+
+
+    function typeNextChar() {
+      if (i < words.length) {
+        const char = words.charAt(i);
+        console.log(i)
+        if (char === '|') {
+          setTimeout(typeNextChar, 200); // Sleep for 200ms (0.2 seconds)
+        } else {
+          setTimeout(typeNextChar, 10); // Normal speed for other characters
+          outputElement.textContent += char;
+        }
+
+        i++;
+      }
+      if (i == 707){
+        goToTerminal()
+      }
+     
+    }
+    typeNextChar();
+    
+    function goToTerminal(){
+      const url = "/html/terminal.html"
+      document.location.href = url;
+    }
---
+++ js/terminal.js
@@ -0,0 +1,74 @@
+// const text = [`Connecting to remote server...\n`,`Authorization successful.\n\n`, `Welcome to the Terminal.\n\n`,`Type "help" to get started.\n\n`];
+const output = document.getElementById('outputs')
+const text = ["I'm still working on this project\n",]
+function connectingTerminal() {
+  function typeLine(lineIndex) {
+    const spn = document.createElement('span');
+    spn.setAttribute('class', 'output-element');
+    output.appendChild(spn);
+    
+    let charIndex = 0;
+    function typeChar() {
+      if (charIndex < text[lineIndex].length) {
+        spn.innerHTML += text[lineIndex][charIndex];
+        charIndex++;
+        setTimeout(typeChar, 50); // Adjust the delay as needed (e.g., 50 milliseconds)
+      } else {
+        // Move to the next line if there are more lines
+        if (lineIndex < text.length - 1) {
+          setTimeout(() => typeLine(lineIndex + 1), 1000); // Delay between lines (e.g., 1000 milliseconds)
+        }
+      }
+    }
+
+    typeChar();
+  }
+  // Clear existing content before typing
+  output.innerHTML = '';
+
+  // Add the entire text to the output element
+  // for (let i = 0; i < text.length; i++) {
+  //   const spn = document.createElement('span');
+  //   spn.setAttribute('class', 'output-element');
+  //   spn.innerHTML = text[i];
+  //   output.appendChild(spn);
+  // }
+
+  // Start typing from the first line
+  typeLine(0);
+}
+connectingTerminal()
+
+// connectingTerminal()
+function userIp(){
+  const e = document.getElementById('label-input')
+  fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => e.innerHTML = `${data.ip}` + "@guest>");
+}
+userIp()
+
+function sendCommand(e){
+  const outputs = document.getElementById('outputs')
+  e = e || window.event;
+  if (e.keyCode === 13){
+    var spanElement = document.createElement("span");
+    const input = document.getElementById('inner-input')
+    spanElement.className = "output-element";
+    spanElement.textContent = input.value;
+    outputs.appendChild(spanElement, outputs.firstChild);
+    if (input.value === "cls"){
+      commands['cls']()
+    }
+    input.value = ""
+  }
+}
+
+const commands = {
+  "cls": () => {
+    const list = document.getElementById("outputs");
+    while (list.hasChildNodes()) {
+      list.removeChild(list.firstChild);
+    }
+  }
+}
